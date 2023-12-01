b1c1759e782b02de814dc787b2fc38ce69de518e
ABlam87 <adamblampied@gmail.com>
2023-12-01 19:41:44 +0000

    highscores now pull from local storage

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -4,6 +4,11 @@
 //display name and score
 //getdata from local storage
 
+var scoresList = JSON.parse(localStorage.getItem("allScores"));
+var scoresTable = document.querySelector('#highscores');
+console.log(scoresList)
 
-
-
+for (i = 0; i < scoresList.length; i++) {
+list = document.createElement("li");
+list.textContent = scoresList[i].Initials + ': ' + scoresList[i].Score;
+scoresTable.appendChild(list);}
