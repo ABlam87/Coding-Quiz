213afbb97939c95b07116470f6b075052f3d97b3
ABlam87 <adamblampied@gmail.com>
2023-12-01 14:15:12 +0000

    added pseudocode

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,33 @@
+//Pseudocode
+
+//setinterval function with a timer counting from 60 to 0
+// if timer === 0
+// timer saved to localstorage
+// highscores.html
+
+// render function 
+// for loop
+// if i < 10
+// append variable to html
+// if i > 10
+// timer saved to local storage
+
+// each question it's own div element with a number id
+// div -> h2 + ol -> li x 4
+
+// list elements have value of 'right' or 'wrong'
+
+// event listener on ol
+
+//if event target == right:
+// - correct sfx
+// - 'Correct!' appears onscreen for 1 second
+// render function
+
+//else 
+// - incorrect sfx
+// - 'Wrong!' appears onscreen for 1 second
+// timer decreased by 10
+// render function
+
+//
---
+++ starter/assets/js/scores.js
@@ -0,0 +1,5 @@
+//pseudocode
+//enter initials form
+//saves name to localstorage
+//display name and score
+//getdata from local storage
